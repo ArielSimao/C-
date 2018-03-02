70b1b15d3d7b97c46b8a306b9f5d3c19b843a3ef
ArielSimao <arielsimao@outlook.com>
2018-03-02 20:18:08 -0300

    Aula

---
+++ salario-wpf-oop-master/ProjetoSalario/Funcionario.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace ProjetoSalario
+{
+    class Funcionario
+    {
+        private static readonly int Comissao = 1;
+        private static readonly int Imposto = 3;
+        private static readonly int AjudaCusto = 200;
+        private static readonly int ValeTranspote = 50;
+
+        public string nome { get; set; }
+        public double salBruto { get; set; }
+        public double adicionais { get; private set; }
+        public double descontos { get; private set; }
+
+        private Boolean imposto;
+
+        public Boolean impostoProp
+        {
+            get { return imposto; }
+            set
+            {
+                this.imposto = value;
+                calcularDescontos();
+            }
+        }
+
+        private Boolean valeTransporte;
+
+        public Boolean valeTransporteProp
+        {
+            get { return this.valeTransporte; }
+            set
+            {
+                this.valeTransporte = value;
+                calcularDescontos();
+            }
+        }
+
+        private Boolean comissao;
+
+        public Boolean comissaoProp
+        {
+            get { return this.comissao; }
+            set
+            {
+                this.comissao = value;
+                calcularAdicionais();
+            }
+        }
+
+        private Boolean ajudaCusto;
+
+        public Boolean ajudaCustoProp
+        {
+            get { return ajudaCusto; }
+            set
+            {
+                this.ajudaCusto = value;
+                calcularAdicionais();
+            }
+        }
+
+        private double salarioLiquido;
+
+        public double salarioLiquidoProp
+        {
+            get { return this.salBruto + this.adicionais - this.descontos; }
+            set { salarioLiquido = value; }
+        }
+
+        private void calcularDescontos()
+        {
+            this.descontos = 0;
+
+            if (this.valeTransporte)
+            {
+                this.descontos += ValeTranspote;
+            }
+
+            if (this.imposto)
+            {
+                this.descontos += (this.salBruto / 100 * Imposto);
+            }
+        }
+
+        private void calcularAdicionais()
+        {
+            this.adicionais = 0;
+
+            if (this.ajudaCusto)
+            {
+                this.adicionais += AjudaCusto;
+            }
+
+            if (this.comissao)
+            {
+                this.adicionais += (this.salBruto / 100 * Comissao);
+            }
+        }
+    }
+}
---
+++ salario-wpf-oop-master/ProjetoSalario/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+
+namespace ProjetoSalario
+{
+    public partial class MainWindow : Window
+    {
+        List<Funcionario> funcionarios = new List<Funcionario>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            Funcionario funcionario;
+
+            try { funcionario = obterFuncionario(); }
+            catch (FormatException fe)
+            {
+                MessageBox.Show(fe.Message, "Salário inválido inválido", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            funcionarios.Add(funcionario);
+            dtgFuncionarios.ItemsSource = null;
+            dtgFuncionarios.ItemsSource = funcionarios;
+
+            lblSarioFixo.Content = "R$ " + funcionario.salarioLiquidoProp.ToString("N", new CultureInfo("pt-BR"));
+        }
+
+        private Funcionario obterFuncionario()
+        {
+            Funcionario funcionario = new Funcionario();
+            funcionario.nome = txtNome.Text;
+            funcionario.salBruto = Double.Parse(txtSalarioBruto.Text);
+
+            funcionario.comissaoProp = rdbComissao.IsChecked.Value;
+            funcionario.ajudaCustoProp = rdbAjudaCusto.IsChecked.Value;
+            funcionario.impostoProp = chkImposto.IsChecked.Value;
+            funcionario.valeTransporteProp = chkValeTransporte.IsChecked.Value;
+
+            return funcionario;
+        }
+
+        private void btnLimpar_Click(object sender, RoutedEventArgs e)
+        {
+            txtNome.Clear();
+            txtSalarioBruto.Clear();
+            rdbAjudaCusto.IsChecked = false;
+            rdbComissao.IsChecked = false;
+            chkImposto.IsChecked = false;
+            chkValeTransporte.IsChecked = false;
+            lblSarioFixo.Content = "";
+
+        }
+    }
+}
